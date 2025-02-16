444690fbc98aee6fad85b55f1e3886e424e66236
Ahmed-Zaman <kanwarghazizaman@gmail.com>
2025-02-17 00:25:18 +0500

    Improved Error handling and Optimization
     * Added transaction block around insert statement
     * Added lazy enumeration while reading csv file
     * Added begin rescue blocks to catch exceptions and loggers to log the errors

--- app/services/import_blogs_service.rb
+++ app/services/import_blogs_service.rb
@@ -6,20 +6,46 @@ class ImportBlogsService
   def initialize(file_path, user_id)
     @file_path = file_path
     @user_id = user_id
+    @logger = ActiveSupport::Logger.new(Rails.root.join('log', 'import_blogs.log'))
   end
 
   def call
     blogs = []
+    row_count = 0
 
-    CSV.foreach(@file_path, headers: true, encoding: 'utf8') do |row|
-      blogs << row.to_h.merge(user_id: @user_id)
+    CSV.foreach(@file_path, headers: true, encoding: 'utf8').lazy.each_slice(BATCH_SIZE) do |rows|
+      rows.each do |row|
+        begin
+          blogs << row.to_h.merge(user_id: @user_id)
+          row_count += 1
+        rescue StandardError => e
+          @logger.error "Error processing row: #{row.inspect}. Error: #{e.message}"
+        end
+      end
 
-      if blogs.size >= BATCH_SIZE
-        Blog.insert_all(blogs)
+      begin
+        insert_batch(blogs)
+      rescue StandardError => e
+        @logger.error "Error inserting batch: #{e.message}"
+      ensure
         blogs.clear
       end
     end
 
-    Blog.insert_all(blogs) unless blogs.empty? # Insert remaining records
+    # Insert any remaining records
+    insert_batch(blogs) unless blogs.empty?
+    @logger.info "Successfully imported #{row_count} rows."
+  end
+
+  private
+
+  def insert_batch(blogs)
+    return if blogs.empty?
+
+    Blog.transaction do
+      Blog.insert_all(blogs)
+    end
+  rescue StandardError => e
+    @logger.error "Failed to insert batch: #{e.message}"
   end
 end
